dfd713411d845b0146f607bc1658ee028489a0b5
confirm <ayaanpathanra@gmail.com>
2026-04-29 17:28:35 +0530

    "Modify User"

--- Backend/auth-app/auth-app/src/main/java/com/auth/auth_app/models/User.java
+++ Backend/auth-app/auth-app/src/main/java/com/auth/auth_app/models/User.java
@@ -34,7 +34,9 @@ public class User {
     @Enumerated(EnumType.STRING)
     private Provider provider = Provider.LOCAL;
 
-    @
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_roles",joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
    private Set<Role> roles = new HashSet<>();
 
 }
