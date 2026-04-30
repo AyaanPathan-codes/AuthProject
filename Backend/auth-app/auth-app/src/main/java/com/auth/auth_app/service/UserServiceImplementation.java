ade60cac82fee4be7ff61471f523ab2a04c24748
confirm <ayaanpathanra@gmail.com>
2026-04-30 14:44:21 +0530

    "Add User Implementation"

---
+++ Backend/auth-app/auth-app/src/main/java/com/auth/auth_app/service/UserServiceImplementation.java
@@ -0,0 +1,42 @@
+package com.auth.auth_app.service;
+
+import com.auth.auth_app.dtos.UserDto;
+
+import java.util.List;
+
+public class UserServiceImplementation implements UserService {
+    @Override
+    public UserDto getUser() {
+        return null;
+    }
+
+    @Override
+    public UserDto getUserById() {
+        return null;
+    }
+
+    @Override
+    public UserDto getUserByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public UserDto createUser(UserDto userDto) {
+        return null;
+    }
+
+    @Override
+    public UserDto updateUser(UserDto userDto) {
+        return null;
+    }
+
+    @Override
+    public void deleteUser(UserDto userDto) {
+
+    }
+
+    @Override
+    public List<UserDto> getAllUsers() {
+        return List.of();
+    }
+}
