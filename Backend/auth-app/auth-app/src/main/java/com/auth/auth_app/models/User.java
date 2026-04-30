5b9b7eb8761b3000b74b651c0f46559b0e2be83a
confirm <ayaanpathanra@gmail.com>
2026-04-30 16:07:13 +0530

    "Change in user entity"

--- Backend/auth-app/auth-app/src/main/java/com/auth/auth_app/models/User.java
+++ Backend/auth-app/auth-app/src/main/java/com/auth/auth_app/models/User.java
@@ -1,6 +1,7 @@
 package com.auth.auth_app.models;
 
 import com.auth.auth_app.enums.Provider;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -32,7 +33,14 @@ public class User {
     private String role;
     private Boolean enabled;
     private String image;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING,
+            pattern = "yyyy-MM-dd HH:mm:ss",
+            timezone = "Asia/Kolkata")
     private Instant createdAt =  Instant.now();
+    @JsonFormat(shape = JsonFormat.Shape.STRING,
+            pattern = "yyyy-MM-dd HH:mm:ss",
+            timezone = "Asia/Kolkata")
     private Instant updatedAt =  Instant.now();
     @Enumerated(EnumType.STRING)
     private Provider provider = Provider.LOCAL;
@@ -41,6 +49,17 @@ public class User {
     @JoinTable(name = "user_role",joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
    private Set<Role> roles = new HashSet<>();
+
+    @PrePersist
+    public void onCreate() {
+        this.createdAt = Instant.now();
+        this.updatedAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void onUpdate() {
+        this.updatedAt = Instant.now();
+    }
 
 }
 
