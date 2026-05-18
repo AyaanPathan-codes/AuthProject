9402739c2ca613dfc28d86ac047b37a8c9509e87
confirm <ayaanpathanra@gmail.com>
2026-05-18 12:19:27 +0530

    "Add Auth implementation"

--- Backend/auth-app/auth-app/src/main/java/com/auth/auth_app/service/implementation/AuthServiceImplementation.java
+++ Backend/auth-app/auth-app/src/main/java/com/auth/auth_app/service/implementation/AuthServiceImplementation.java
@@ -3,8 +3,17 @@ package com.auth.auth_app.service.implementation;
 
 import com.auth.auth_app.dtos.UserDto;
 import com.auth.auth_app.service.AuthService;
+import com.auth.auth_app.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
+
+@Service
+@AllArgsConstructor
 public class AuthServiceImplementation implements AuthService {
+
+
+    private final UserService userService;
 
     @Override
     public UserDto registerUser(UserDto userDto) {
